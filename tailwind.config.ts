e194f2149126f04f13455289bb83b880aa75b13b
Bgmi Id <bgmihackbro@gmail.com>
2025-09-15 16:20:30 +0000

    text of app converts in times new roman

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Times New Roman', 'serif'],
+        headline: ['Times New Roman', 'serif'],
         code: ['monospace'],
       },
       colors: {
