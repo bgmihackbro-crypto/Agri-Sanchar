37c29d88dbc347b55cb78e06936894c0d9f1b471
Bgmi Id <bgmihackbro@gmail.com>
2025-09-19 17:49:01 +0000

    use this text search in a community (_for element <Input>_)

--- src/app/(app)/community/page.tsx
+++ src/app/(app)/community/page.tsx
@@ -1135,7 +1135,7 @@ export default function CommunityPage() {
                 <div className="relative flex-1">
                     <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                     <Input 
-                        placeholder="Search community..." 
+                        placeholder="Search in a community" 
                         className="pl-9"
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
